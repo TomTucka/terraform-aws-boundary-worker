b73e80fbd231d64aa6eafa9baca39b801f2893c1
tomtucka <tomtucka@gmail.com>
2023-10-07 17:10:40 +0100

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "unique_name" {
+  type        = string
+  description = "Workers unique name"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "aws_vpc" {
+  type = string
+}
+
+variable "aws_ami" {
+  type = string
+}
+
+variable "aws_public_secgroup_id" {
+  type = string
+}
+
+variable "app_infra_ssh_privkey" {
+  type = string
+}
+
+variable "boundary_worker_subnet_id" {
+  type = string
+}
+
+variable "worker_initial_upstreams" {
+  type = list(string)
+}
+
+variable "boundary_cluster_admin_url" {
+  type = string
+}
+
+variable "boundary_worker_instance_type" {
+  type    = string
+  default = "t3.small"
+}
