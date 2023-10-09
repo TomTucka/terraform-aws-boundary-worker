78104f2fdab295919419cac3c0385f0d40fccdb1
tomtucka <tomtucka@gmail.com>
2023-10-09 10:16:32 -0700

    update gpg key, output ssh key

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "boundary_worker_dns_public" {
   description = "The public DNS of the Boundary worker."
   value       = aws_instance.boundary_worker.public_dns
 }
+
+output "boundary_infra_ssh_privkey" {
+  description = "The raw content of the Boundary worker SSH private key."
+  value       = tls_private_key.boundary_instance_worker_ssh_key.private_key_openssh
+}
